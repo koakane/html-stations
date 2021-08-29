4fb91a1d4641c912dec004b3b62be3d7e6d965be
nogata <yasuaketaka5588@gmail.com>
2021-08-30 08:47:44 +0900

    s15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,24 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+
+  await delay(3);
+  const result = await userList.map(test)
   return await result
+  
 }
 
-function test() {
-  return
+function delay(n){
+  return new Promise(function(resolve){
+      setTimeout(resolve,n*1000);
+  });
 }
+
+function test(data) {
+  data.full_name = data.family_name + ' ' +data.first_name;
+  
+  return data
+}
+
+
 
